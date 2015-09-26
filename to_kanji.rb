fee3127042fc598d68bd59c16d3989f0dd506cc5
rawhide.suzukih <rawhide.suzukih@gmail.com>
2015-09-26 17:58:07 +0900

    2

--- to_kanji.rb
+++ to_kanji.rb
@@ -1,33 +1,45 @@
-class Fixnum
+class Integer
   def to_kanji
-    [].tap do |kanji|
-      self.to_s.split("").map(&:to_i)
-                       .reverse.each_with_index do |figure, digit|
-        kanji << (figure.zero? ? figure_table(digit)[figure] : figure_table(digit)[figure]+digit_table[digit])
-      end
-      kanji.reverse.join("")
+    fixnum_kanji = []
+    self.reversed_numerical_array.each_with_index do |figure, digit|
+      kanji = figure_table(digit)[figure]
+      kanji += digit_table[digit] unless figure.zero?
+      fixnum_kanji << kanji
     end
+    fixnum_kanji.reverse.join("")
   end
 
-  private
-    def figure_table(digit)
-      {
-        0=>"",   1=>(digit == 0 ? "一" : ""), 2=>"二", 3=>"三",
-        4=>"四", 5=>"五", 6=>"六", 7=>"七",
-        8=>"八", 9=>"九"
-      }
-    end
+protected
+  def chars
+    self.to_s.chars
+  end
 
-    def digit_table()
-      base_unit     = ["", "十", "百", "千"]
-      ["", "万", "億", "兆", "京"].map do |elem|
-      end
-=begin
-      [
-        "", "十", "百", "千", "万", "十万", "百万", "千万",
-        "億", "十億", "千億", "兆", "十兆", "百兆", "千兆",
-        "京", "十京", "百京", "千京"
-      ]
-=end
-    end
+  def reversed_numerical_array
+    self.chars.map(&:to_i).reverse
+  end
+
+  def figure_table(digit)
+    {
+      0=>""  , 1=>one_kanji(digit),
+      2=>"二", 3=>"三",
+      4=>"四", 5=>"五",
+      6=>"六", 7=>"七",
+      8=>"八", 9=>"九"
+    }
+  end
+
+  def one_kanji(digit)
+    (0..3).include?(digit) ? "" : "一"
+  end
+
+  def digit_table
+    base_units   = ["", "十", "百", "千"]
+    extend_units = ["", "万", "億", "兆", "京"]
+    digit_table = extend_units.map do |eu|
+                    base_units.map do |bu|
+                      bu + eu
+                    end
+                  end
+    digit_table.flatten
+  end
 end
